62bfe23cd1c49e6802339a1b6176abb170cc6d74
Rewat Boonsit <ravatna@gmail.com>
2016-09-19 11:59:09 +0700

    init commit

---
+++ AMIThai/src/com/projectth/model/ListPlaceController.java
@@ -0,0 +1,28 @@
+package com.projectth.model;
+
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.Executions;
+import org.zkoss.zk.ui.event.Event;
+import org.zkoss.zk.ui.select.SelectorComposer;
+import org.zkoss.zk.ui.select.annotation.Listen;
+import org.zkoss.zul.Window;
+
+public class ListPlaceController extends SelectorComposer<Component> {
+	private static final long serialVersionUID = 1L;
+	
+	@Listen("onClick = #addNewBtn")
+	public void showModal(Event e) {
+
+		Window window = (Window)Executions.createComponents(
+				"/widgets/window/modal_dialog/place_dialog.zul", null, null);
+		window.doModal();
+	}
+	
+//	@Listen("onClick = #photoGalleryBtn")
+//	public void showModalPhotoGallery(Event e){
+//		Window window = (Window)Executions.createComponents("/widgets/window/modal_dialog/hotel_dialog.zul",null,null);
+//		window.doModal();
+//	}
+	
+	
+}
